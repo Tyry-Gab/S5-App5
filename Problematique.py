948e8f878efcac1cc7bf458012371572c5f40e9f
glesssard <lesg2605@usherbrooke.ca>
2019-11-14 15:10:31 -0500

    Changed everything into functions

--- Problematique.py
+++ Problematique.py
@@ -9,48 +9,60 @@ import scipy.misc
 
 # Getting poles and zeros from aberrations transfer function
 ########################################################################################################################
-zeros = [
+
+Zeros = [
     0.9*np.exp(np.complex(0, np.pi/2)),
     0.9*np.exp(np.complex(0, -np.pi/2)),
     0.95*np.exp(np.complex(0, np.pi/8)),
     0.95*np.exp(np.complex(0, -np.pi/8))
 ]
 
-poles = [
+Poles = [
     0,
     -0.99,
     -0.99,
     0.8
 ]
 
-angles = np.linspace(0,2*np.pi,1000)
-plt.figure()
-for zero in zeros:
-    plt.scatter(np.real(zero), np.imag(zero), marker="o", c='b')
-for pole in poles:
-    plt.scatter(np.real(pole), np.imag(pole), marker="x", c='r')
-plt.plot(np.cos(angles),np.sin(angles))
-plt.show()
+
+def scatter_poles_zeros(poles, zeros):
+    angles = np.linspace(0,2*np.pi,1000)
+    plt.figure()
+    for zero in zeros:
+        plt.scatter(np.real(zero), np.imag(zero), marker="o", c='b')
+    for pole in poles:
+        plt.scatter(np.real(pole), np.imag(pole), marker="x", c='r')
+    plt.plot(np.cos(angles),np.sin(angles))
+    plt.show()
 
 
 # Designing reverse filter for aberrations
 ########################################################################################################################
-b = [1, -1.75537, 1.7125, -1.42185, 0.73105]
-a = [1, 1.18, -0.6039, -0.78408, 0]
+
+def transfer_from_poles_zeros(poles,zeros,show=False):
+    b = np.poly(zeros)
+    a = np.poly(poles)
+
+    if show:
+        plt.figure()
+        x,y = signal.freqz(b,a)
+        plt.plot(x, 20*np.log10(abs(y)))
+        plt.show()
+    return b,a
 
 
+def remove_aberrations(poles,zeros, source_image):
+    aberration = np.load(source_image)
+    b,a = transfer_from_poles_zeros(poles,zeros)
+    plt.figure()
+    output=signal.lfilter(a, b, aberration)
+    plt.imshow(output,cmap='gray')
+    plt.show()
+    return output
 
-plt.figure()
-x,y = signal.freqz(b,a)
-plt.plot(x, 20*np.log10(abs(y)))
-plt.show()
-
-
-aberration = np.load('pictures/goldhill_aberrations.npy')
-
-plt.figure()
-output=signal.lfilter(a, b, aberration)
-plt.imshow(output,cmap='gray')
-plt.show()
-
+# Desingning rotating transform matrix
 ########################################################################################################################
+
+
+if __name__ == "__main__":
+    remove_aberrations(Poles,Zeros,'pictures/goldhill_aberrations.npy')
