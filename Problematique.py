a10d3c9ed6175d72122111011cf8cde1f2394c64
glesssard <lesg2605@usherbrooke.ca>
2019-11-18 10:44:20 -0500

    Final changes

--- Problematique.py
+++ Problematique.py
@@ -45,10 +45,11 @@ def transfer_from_poles_zeros(poles,zeros,show=False):
     return b,a
 
 
-def remove_aberrations(poles,zeros, source_image, show=False):
-    aberration = np.load(source_image)
+def remove_aberrations(poles,zeros, source_image, npy=False, show=False):
+    if npy:
+        source_image = np.load(source_image)
     b,a = transfer_from_poles_zeros(poles,zeros)
-    output = signal.lfilter(a, b, aberration)
+    output = signal.lfilter(a, b, source_image)
     if show:
         plt.figure()
         plt.imshow(output,cmap='gray')
@@ -61,7 +62,7 @@ def rotate_image(source_image, npy=False, show=False):
     if npy:
         img = np.load(source_image)
     else:
-        img = np.array(Image.open(source_image))
+        img = source_image
 
     new_img = np.empty(shape=(img.shape[1], img.shape[0]))
 
@@ -95,13 +96,11 @@ def rotate_image(source_image, npy=False, show=False):
 def noise_removal_bilinear(source_image, npy=True, show=False):
     if npy:
         source_image = np.load(source_image)
-    butter_order = 2
-    Fp = 650
-    Fc = 750
+    Fc = 650
     Fe = 1600
     T = 1/Fe
 
-    wc = Fe*np.tan(np.pi*Fp/Fe)
+    wc = Fe*np.tan(np.pi*Fc/Fe)
 
     b = [
         T**2*wc**2,
@@ -115,6 +114,8 @@ def noise_removal_bilinear(source_image, npy=True, show=False):
     ]
     a = np.asarray(a)
     b = np.asarray(b)
+    print(a)
+    print(b)
     if show:
         plt.figure()
         x,y = signal.freqz(b,a)
@@ -141,8 +142,9 @@ def noise_removal_cheat(source_image, npy=True, show=False):
     Fc = 750
     Fe = 1600
 
-    order, Wn = signal.buttord(Fp/Fe, Fc/Fe, gpass=0.5, gstop=40)
-    order = 2
+    order, Wn = signal.buttord(2*Fp/Fe, 2*Fc/Fe, gpass=0.5, gstop=40)
+    print(order)
+    #order = 2
     b, a, *_ = signal.butter(order, Wn)
 
     if show:
@@ -161,22 +163,53 @@ def noise_removal_cheat(source_image, npy=True, show=False):
     return output
 
 
+def compress(image_array, show=False, N=69.420):
+    covariance_matrix = np.cov(image_array)
+    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    compressed_img = eigenvectors.dot(image_array)
+
+    if show:
+        plt.figure()
+        plt.imshow(compressed_img, cmap='gray')
+        plt.show()
+
+    i = 0
+    threshold = N
+    counter = 0
+    for line in compressed_img:
+        mean = np.average(100*np.abs(compressed_img[i]))
+        if mean <= threshold:
+            line.fill(0)
+            counter += 1
+        i += 1
+    if show:
+        print("There were {0} lines removed".format(counter))
+
+    return compressed_img, eigenvectors
+
+
+def decompress(cm_img, eig_vectors, show=False):
+    decompressed = np.linalg.inv(eig_vectors).dot(cm_img)
+    if show:
+        plt.figure()
+        plt.imshow(decompressed, cmap='gray')
+        plt.show()
+    return decompressed
+
 
 ########################################################################################################################
 if __name__ == "__main__":
 
-    cleaned = remove_aberrations(Poles,Zeros,'pictures/goldhill_aberrations.npy', show=True)
-    scipy.misc.imsave('pictures/main/aberrations.jpg', cleaned)
+    # Cleaning up image
+    cleaned = remove_aberrations(Poles,Zeros,'pictures/image_complete.npy', npy=True)
+    turned = rotate_image(cleaned, npy=False)
+    final = noise_removal_bilinear(turned,npy=False, show=True)
+    scipy.misc.imsave('pictures/main/cleaned_up.jpg', final)
 
-    noise_bilinear = noise_removal_bilinear('pictures/goldhill_bruit.npy', show=True)
-    noise_cheat = noise_removal_cheat(cleaned, npy=False, show=True)
-    #Returns the np.array of the rotated picture
-    rotate_image('pictures/goldhill_rotate_source.png', show=True)
+    compressed, vectors = compress(final, N=26.420)
+    guess_whos_back = decompress(compressed, vectors, show=True)
 
 
-    scipy.misc.imsave('pictures/main/noise.jpg', noise)
-
-    rotate_image(None)
 
 
 
